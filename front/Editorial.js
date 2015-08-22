1dcbfb65c3497c33a144ba8cb4159dfb43a7d0d2
Jared Forsyth <jared@jaredforsyth.com>
2015-08-22 08:38:53 -0600

    more editor styling

--- front/Editorial.js
+++ front/Editorial.js
@@ -6,13 +6,22 @@ var TYPES = ['underline', 'highlight', 'sideline'];
 
 export default class Editorial {
   render() {
+    var currentColor = this.props.mark.getIn(['style', 'color']);
     var colors = COLORS.map(color => (
-      <li style={styles.color} onClick={() => this.props.setMarkColor(color)}>
+      <li style={{...styles.color, backgroundColor: color, ...(color === currentColor && styles.currentColor)}} onClick={() => this.props.setMarkColor(color)}>
         {color}
       </li>
     ));
+    var currentType;
+    if (this.props.mark.get('type') === 'sideline') {
+      currentType = 'sideline';
+    } else if (this.props.mark.getIn(['style', 'underline'])) {
+      currentType = 'underline';
+    } else {
+      currentType = 'highlight';
+    }
     var types = TYPES.map(type => (
-      <li style={styles.type} onClick={() => this.props.setMarkStyle(type)}>
+      <li style={{...styles.type, ...(type === currentType && styles.currentType)}} onClick={() => this.props.setMarkStyle(type)}>
         {type}
       </li>
     ));
@@ -46,23 +55,30 @@ var styles = {
   },
   colors: {
     listStyle: 'none',
-    margin: 0,
+    margin: '0 5px',
     padding: 0,
   },
   types: {
     listStyle: 'none',
-    margin: 0,
+    margin: '0 5px',
     padding: 0,
   },
   color: {
     display: 'inline-block',
     padding: '5px 10px',
     cursor: 'pointer',
+    opacity: .4,
+  },
+  currentColor: {
+    opacity: 1,
   },
   type: {
     display: 'inline-block',
     padding: '5px 10px',
     cursor: 'pointer',
+  },
+  currentType: {
+    backgroundColor: '#555',
   },
 };
 
