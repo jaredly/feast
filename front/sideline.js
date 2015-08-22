1fafa7d30e888ef4bc83c359e06b9cb68aa72157
Jared Forsyth <jared@jaredforsyth.com>
2015-08-22 08:51:30 -0600

    highlight sideline when editing

--- front/sideline.js
+++ front/sideline.js
@@ -2,16 +2,29 @@
  * @flow
  */
 
-import type {Context, Mark} from './types';
+import type {Context, Mark, FontConfig} from './types';
 
-export default function sideline(ctx: Context, mark: Mark, coords: {top: number, left: number, bottom: number}) {
-  ctx.globalAlpha = 1;
-  ctx.beginPath();
-  ctx.strokeStyle = mark.style.color;
-  ctx.lineWidth = 2;
+export default function sideline(ctx: Context, mark: Mark, font: FontConfig, coords: {top: number, left: number, bottom: number}, editing: bool) {
+
   var {left, top, bottom} = coords;
-  ctx.moveTo(left, top);
-  ctx.lineTo(left, bottom);
-  ctx.stroke();
+  ctx.globalAlpha = 1;
+
+  if (editing) {
+    ctx.beginPath();
+    ctx.strokeStyle = mark.style.color
+    ctx.lineWidth = font.space / 2;
+    ctx.moveTo(left, top);
+    ctx.lineTo(left, bottom);
+    ctx.stroke();
+  } else {
+
+
+    ctx.beginPath();
+    ctx.strokeStyle = mark.style.color;
+    ctx.lineWidth = 2;
+    ctx.moveTo(left, top);
+    ctx.lineTo(left, bottom);
+    ctx.stroke();
+  }
 }
 
