b96f98567352fdd31286515a3f0a23affe09240e
Jared Forsyth <jared@jaredforsyth.com>
2015-09-17 21:21:55 -0600

    some stress testing

--- sync/front/__tests__/Shared.js
+++ sync/front/__tests__/Shared.js
@@ -8,6 +8,9 @@ import {pit, pwait, pcheck, prom, fakeDb, socketPair, laggySocketPair, randomSoc
 import {expect} from 'chai';
 import chalk from 'chalk';
 
+function rand(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
 
 function rebaser(actions, oldTail, newTail) {
   return actions.map(({id, action: {name}}) => ({id, action: {name: name + '+'}}));
@@ -195,7 +198,7 @@ describe('Shared.js', () => {
     expect((await db.dumpData()).data).to.eql(goalData, 'db dump');
   });
 
-  pit.only('two shared, four tabs, all the rebases', async () => {
+  pit('two shared, four tabs', async () => {
     var remote = new LaggyMemRemote(reducer, [{id: 'first', action: {name: 'thefirst'}}], 10);
 
     var db1 = fakeDb(reducer, null, null);
@@ -240,7 +243,116 @@ describe('Shared.js', () => {
     expect((await db2.dumpData()).data).to.eql(goalData, 'db2 dump');
   });
 
-  pit('many shared, many tabs', async () => {
+  pit('two shared, four tabs, all the rebases', async () => {
+    var remote = new LaggyMemRemote(reducer, [{id: 'first', action: {name: 'thefirst'}}], 10);
+
+    var db1 = fakeDb(reducer, null, null, [{id: 'shared1action', action: {name: 'fromshared1'}}]);
+    var shared1 = new Shared(db1, remote, rebaser, 1);
+    shared1.id = chalk.blue('shared1');
+
+    var db2 = fakeDb(reducer, null, null, [{id: 'shared2action', action: {name: 'fromshared2'}}]);
+    var shared2 = new Shared(db2, remote, rebaser, 1);
+    shared2.id = chalk.green('shared2');
+
+    var shareds = [shared1, shared2];
+    
+    var tabs = [];
+    shareds.forEach((shared, s) => {
+      for (var i=0; i<2; i++) {
+        var n = s * 2 + i;
+        var [tabSock, sharedSock] = socketPair();
+        var tab = new Tab(tabSock, reducer, rebaser);
+        tab.id = chalk.green('tab' + n);
+        shared.addConnection(sharedSock);
+        tabs.push(tab);
+      }
+    });
+
+    await Promise.all(shareds.concat(tabs).map(s => s.init()));
+
+    tabs[0].addAction({name: 'hello'});
+    tabs.forEach((tab, i) => tab.addAction({name: 'from' + i}));
+
+    await pwait(100);
+    expect(shared1.state.serverHead).to.equal(remote.head, 'shared serverHead');
+    expect(shared2.state.serverHead).to.equal(remote.head, 'shared serverHead');
+    var goalData = remote.actions.reduce(reducer, null);
+    tabs.forEach((tab, i) => {
+      expect(tab.state.serverHead).to.equal(remote.head, 'tab serverHead' + i);
+
+      expect(tab.state.local).to.eql(goalData, 'tab-local' + i);
+      expect(tab.state.shared).to.eql(goalData, 'tab-shared' + i);
+      expect(tab.state.server).to.eql(goalData, 'tab-server' + i);
+    });
+    expect((await db1.dumpData()).data).to.eql(goalData, 'db1 dump');
+    expect((await db2.dumpData()).data).to.eql(goalData, 'db2 dump');
+  });
+
+  pit.only('many shared, many tabs', async () => {
+    var stored = [];
+    var num_stored = rand(4, 10);
+    for (var i=0; i<num_stored; i++) {
+      stored.push({
+        id: 'stored' + i,
+        action: {name: 'fromstored' + i}
+      });
+    }
+    var remote = new LaggyMemRemote(reducer, stored, 10);
+
+    var NUM_SHARED = 10;
+    var NUM_TABS = 4;
+
+    var dbs = [];
+    var shareds = [];
+    var tabs = [];
+    for (var i=0; i<NUM_SHARED; i++) {
+      var pending = [];
+      var num_pending = rand(2, 6);
+      for (var p=0; p<num_pending; p++) {
+        pending.push({id: 'shared' + i + 'pending' + p, action: {
+          name: 'from' + i + ':' + p
+        }});
+      }
+      var db = fakeDb(reducer, null, null, pending);
+      var shared = new Shared(db, remote, rebaser, 1);
+      shared.id = chalk.blue('shared' + i);
+      shareds.push(shared);
+      dbs.push(db);
+
+      for (var t=0; t<NUM_TABS; t++) {
+        var n = i * NUM_TABS + t;
+        var [tabSock, sharedSock] = socketPair();
+        var tab = new Tab(tabSock, reducer, rebaser);
+        tab.id = chalk.green('tab' + n);
+        shared.addConnection(sharedSock);
+        tabs.push(tab);
+      }
+    }
+
+    await Promise.all(shareds.concat(tabs).map(s => s.init()));
+
+    tabs[0].addAction({name: 'hello'});
+    tabs.forEach((tab, i) => setTimeout(() => {
+      tab.addAction({name: 'tabfrom' + i})
+    }, rand(10, 30)));
+
+    await pwait(200);
+    shareds.forEach((shared, s) => {
+      expect(shared.state.serverHead).to.equal(remote.head, 'shared serverHead' + s);
+    });
+
+    var goalData = remote.actions.reduce(reducer, null);
+    tabs.forEach((tab, i) => {
+      expect(tab.state.serverHead).to.equal(remote.head, 'tab serverHead' + i);
+
+      expect(tab.state.local).to.eql(goalData, 'tab-local' + i);
+      expect(tab.state.shared).to.eql(goalData, 'tab-shared' + i);
+      expect(tab.state.server).to.eql(goalData, 'tab-server' + i);
+    });
+    for (var i=0; i<dbs.length; i++) {
+      var db = dbs[i];
+      expect((await db.dumpData()).data).to.eql(goalData, 'db' + i + ' dump');
+    }
   });
 
   pit('three shared w/ temporary partition', async () => {
